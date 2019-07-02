e8411c8977e7ab061cb6cceec7f15e767febc799
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2019-07-02 13:52:41 +0530

    commited es6 code

---
+++ es6.js
@@ -0,0 +1,468 @@
+// var explanation
+
+var x = 1;
+
+{
+  var x = 2;
+  console.log(x);
+}
+
+{
+  var x = 3;
+}
+
+console.log(x);
+
+// let explanation
+
+let x = 1;
+
+{
+  let x = 2;
+  console.log(x);
+}
+
+console.log(x);
+
+const x = { a: 1, b: 2 };
+
+x.b = 3;
+
+console.log(x);
+
+const firstName = "Yagnesh";
+
+const lastName = "Modh";
+
+// const fullName = firstName + '\'s ' + lastName;
+
+// console.log(fullName);
+
+const fullName = `${firstName}'s        ${lastName}`;
+
+console.log(fullName);
+
+// string literals
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//     <title>Document</title>
+// </head>
+// <body>
+
+// </body>
+// </html>
+
+// const html = "<!DOCTYPE html>" +
+// "\n<html lang=\"en\">" +
+// "\n<head>" +
+//     "\n\t<meta charset=\"UTF-8\">";
+
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+`;
+
+console.log(html);
+
+// spread operator
+
+const x = { a: 1, b: 2 };
+
+const y = { ...x, c: 3 };
+
+console.log(y);
+
+for (const key in x) {
+  if (key === "a") {
+    delete x[key];
+  }
+}
+
+console.log(x);
+
+// destructuring
+
+const x = { a: 1, b: 2, c: 3, d: 4 };
+
+const a = { ...x, c: 3 };
+
+const { a: d, b, ...rest } = x;
+
+console.log(rest);
+
+console.log(d);
+
+const x = [1, 2, 3, 4];
+
+// x.push(5);
+
+// console.log(x);
+
+const y = [0, ...x];
+
+console.log(y);
+console.log(x);
+
+const [, , ...rest] = y;
+console.log(rest);
+
+const aa = 1;
+const bb = "1";
+
+console.log(aa == bb);
+console.log(aa === bb);
+
+const x = [1, 2, 4, 5];
+
+// x.push(5);
+
+// console.log(x);
+
+const y = [0, ...x];
+
+console.log(y);
+console.log(x);
+
+const [, , ...rest] = y;
+console.log(rest);
+
+const index = x.findIndex(function(item) {
+  return item === 2;
+});
+
+console.log(index);
+
+const newArr = [...x.slice(0, index + 1), 3, ...x.slice(index + 1)];
+console.log(newArr);
+
+console.log(x);
+
+const users = [
+  {
+    name: "virat",
+    gender: "male"
+  },
+  {
+    name: "rohit",
+    gender: "female"
+  },
+  {
+    name: "dipika",
+    gender: "female"
+  },
+  {
+    name: "priyanka",
+    gender: "female"
+  }
+];
+
+const newUser = {
+  name: "shikar",
+  gender: "male"
+};
+
+const newUsers = [newUser, ...users];
+
+console.log(newUsers);
+
+const index = newUsers.findIndex(function(x) {
+  return x.name === "rohit";
+});
+
+console.log(index);
+
+const updatedUsers = [
+  ...newUsers.slice(0, index),
+  { ...newUsers[index], gender: "male" },
+  ...newUsers.slice(index + 1)
+];
+
+const lst = newUsers.filter(function(item) {
+  return item.name !== "rohit";
+});
+
+const deletedUsers = [
+  ...newUsers.slice(0, index),
+  ...newUsers.slice(index + 1)
+];
+
+console.log(updatedUsers);
+
+console.log(deletedUsers);
+
+// arrow function
+
+// function abc() {
+//     return 1;
+// }
+
+// console.log(abc());
+
+const add = (a, b) => {
+  const c = 3;
+  return a + b + c;
+};
+
+console.log(add(1, 2));
+
+// class
+
+class Animal {
+  constructor(type = "Animal") {
+    this.type = type;
+  }
+
+  get type() {
+    return this._type;
+  }
+
+  set type(val) {
+    this._type = val.toUpperCase();
+  }
+
+  makeSound() {
+    console.log(this.type);
+    return this.type;
+  }
+}
+
+const a = new Animal();
+
+console.log(a.makeSound());
+
+class Cat extends Animal {
+  constructor() {
+    super("Cat");
+  }
+
+  makeSound() {
+    super.makeSound();
+    console.log("Meow...");
+  }
+}
+
+const b = new Cat();
+
+console.log(b.type);
+console.log(b.makeSound());
+
+class Animal {
+  constructor(type = "Animal") {
+    this.type = type;
+  }
+
+  get type() {
+    return this._type;
+  }
+
+  set type(val) {
+    this._type = val.toUpperCase();
+  }
+
+  makeSound() {
+    // console.log(this.type)
+    // return this.type
+    setTimeout(() => {
+      console.log(this.type);
+    }, 1000);
+  }
+}
+
+const a = new Animal();
+
+console.log(a.makeSound());
+
+class Cat extends Animal {
+  constructor() {
+    super("Cat");
+  }
+
+  makeSound() {
+    super.makeSound();
+    console.log("Meow...");
+  }
+}
+
+const b = new Cat();
+
+console.log(b.type);
+console.log(b.makeSound());
+
+const arr = [1, 2, 3, 4];
+
+console.time("for loop");
+for (let index = 0; index < arr.length; index++) {}
+console.timeEnd("for loop");
+
+console.time("forEach loop");
+arr.forEach(arr => {});
+console.timeEnd("forEach loop");
+
+// map
+
+const users = [
+  {
+    name: "virat",
+    gender: "male"
+  },
+  {
+    name: "rohit",
+    gender: "male"
+  },
+  {
+    name: "dipika",
+    gender: "female"
+  },
+  {
+    name: "priyanka",
+    gender: "female"
+  }
+];
+
+let newUsers = [];
+
+for (let index = 0; index < users.length; index++) {
+  const element = users[index];
+  newUsers = [...newUsers, { ...element, type: "celebrity" }];
+}
+
+const newUsers1 = users.map(x => {
+  if (x.gender === "male") {
+    return { ...x, type: "cricketer" };
+  } else {
+    return { ...x, type: "actor" };
+  }
+});
+
+console.log(users);
+
+console.log(newUsers1);
+
+const users = [
+  {
+    name: "virat",
+    gender: "male"
+  },
+  {
+    name: "rohit",
+    gender: "male"
+  },
+  {
+    name: "dipika",
+    gender: "female"
+  },
+  {
+    name: "priyanka",
+    gender: "female"
+  }
+];
+
+console.time("for loop");
+let newUsers = [];
+for (let index = 0; index < users.length; index++) {
+  const element = users[index];
+  newUsers = [...newUsers, { ...element, type: "celebrity" }];
+}
+console.timeEnd("for loop");
+console.time("map loop");
+const newUsers1 = users.map(x => ({ ...x, type: "celebrity" }));
+console.timeEnd("map loop");
+
+//   const newUsers1 = users.map(x => {
+//      if(x.gender === 'male') {
+//          return {...x, type: 'cricketer'}
+//      } else {
+//         return {...x, type: 'actor'}
+//      }
+
+//   })
+
+//   console.log(users);
+
+//   console.log(newUsers1);
+
+const a = [1, 2, 3, 4, 5];
+
+// let sum = 0;
+
+// for (let index = 0; index < a.length; index++) {
+//     const element = a[index];
+//     sum += element;
+// }
+
+// console.log(sum);
+
+const sum = a.reduce((p, c) => p + c);
+
+console.log(sum);
+
+const a = null;
+
+const b = 2;
+
+const c = a || b;
+
+const d = a && b;
+
+console.log(c);
+console.log(d);
+
+const users = [
+  {
+    name: "virat",
+    gender: "male"
+  },
+  {
+    name: "rohit",
+    gender: "male"
+  },
+  {
+    name: "dipika",
+    gender: "female"
+  },
+  {
+    name: "priyanka",
+    gender: "female"
+  }
+];
+
+//   const abc = {};
+
+//   abc['male'] = [];
+
+//   console.log(abc);
+
+//   {
+//       male: [],
+//       female: []
+//   }
+
+// const groupBy = users.reduce((previous, current) => {
+//     previous[current['gender']] = previous[current['gender']] || [];
+//     previous[current['gender']].push(current);
+//     return previous;
+// }, {});
+
+const groupBy = (arr, key) => {
+  return arr.reduce((previous, current) => {
+    (previous[current[key]] = previous[current[key]] || []).push(current);
+    return previous;
+  }, {});
+};
+
+console.log(groupBy(users, "gender"));
