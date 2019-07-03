65f7cfeddfb7a6eeaafada32edde14fa68789564
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2019-07-03 11:49:28 +0530

    commit settings

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  parser: "babel-eslint",
+  extends: ["airbnb", "prettier", "plugin:react/recommended", "prettier/react"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["babel", "import", "jsx-a11y", "prettier", "react", "react-hooks"],
+  rules: {
+    "react/jsx-filename-extension": [
+      2,
+      {
+        extensions: [".js", ".jsx"]
+      }
+    ]
+  }
+};
--- src/Todo/index.js
+++ src/Todo/index.js
@@ -1,17 +1,92 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-export default class index extends Component {
+export default class Index extends Component {
   state = {
-    todoText: ""
+    todoText: '',
+    todoList: []
+  };
+
+  changeText = event => {
+    this.setState({ todoText: event.target.value });
+  };
+
+  addTodo = event => {
+    event.preventDefault();
+    const { todoText, todoList } = this.state;
+    this.setState({
+      todoList: [
+        ...todoList,
+        {
+          id: todoList.length + 1,
+          text: todoText,
+          isDone: false
+        }
+      ],
+      todoText: ''
+    });
+  };
+
+  markDone = item => {
+    const { todoList } = this.state;
+    const index = todoList.findIndex(x => x.id === item.id);
+    this.setState({
+      todoList: [
+        ...todoList.slice(0, index),
+        { ...item, isDone: !item.isDone },
+        ...todoList.slice(index + 1)
+      ]
+    });
+  };
+
+  deleteTodo = todo => {
+    this.setState(({ todoList }) => ({
+      todoList: todoList.filter(x => x.id !== todo.id)
+    }));
+
+    // this.setState(state => {
+    //   return {
+    //     todoList: state.todoList.filter(x => x.id !== todo.id)
+    //   };
+    // });
   };
 
   render() {
-    const { todoText } = this.state;
+    const { todoText, todoList } = this.state;
     return (
       <>
         <h1>Todo App</h1>
-        <input type="text" placeholder="Write your TODO here..." />
-        <button type="button">Add Todo</button>
+
+        <form onSubmit={this.addTodo}>
+          <input
+            type="text"
+            placeholder="Write your TODO here..."
+            value={todoText}
+            onChange={this.changeText}
+          />
+          <button type="submit">Add Todo</button>
+        </form>
+
+        <div>
+          {todoList.map(todoItem => (
+            <div key={todoItem.id}>
+              <input
+                type="checkbox"
+                value={todoItem.isDone}
+                onClick={() => this.markDone(todoItem)}
+              />
+              <span
+                style={{
+                  textDecoration: todoItem.isDone ? 'line-through' : 'none'
+                }}
+              >
+                {todoItem.text}
+              </span>
+              <button type="button" onClick={() => this.deleteTodo(todoItem)}>
+                Delete
+              </button>
+            </div>
+          ))}
+        </div>
       </>
     );
   }
